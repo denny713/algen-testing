8679d128d4664602d8d5a63a0d4383111ee8a72d
Denny Afrizal <denny.afrizal713@gmail.com>
2023-12-26 23:28:47 +0700

    Project Skeleton

---
+++ src/main/java/com/algen/AlgenPluginApplication.java
@@ -0,0 +1,13 @@
+package com.algen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgenPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algen/AlgenPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgenPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
